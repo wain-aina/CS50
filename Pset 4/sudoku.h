f9c9e797a079c531054cd9233630a87a536e3a83
Olivia Brown <obrown917@gmail.com>
2017-02-10 23:46:10 -0800

    Added Sudoku Part 2 in Pset 4

---
+++ Pset 4/sudoku.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+ * sudoku.h
+ *
+ * CS50 AP
+ * Sudoku (Part 1)
+ *
+ * Compile-time options for the game of Sudoku.
+ ***************************************************************************/
+
+// game's author
+#define AUTHOR "Olivia Brown"
+
+// game's title
+#define TITLE "Sudoku"
+
+// banner's colors
+#define FG_BANNER COLOR_CYAN
+#define BG_BANNER COLOR_WHITE
+
+// grid's colors
+#define FG_GRID COLOR_CYAN
+#define BG_GRID COLOR_WHITE
+
+// border's colors
+#define FG_BORDER COLOR_WHITE
+#define BG_BORDER COLOR_CYAN
+
+// logo's colors
+#define FG_LOGO COLOR_WHITE
+#define BG_LOGO COLOR_BLACK
+
+// digits's colors
+#define FG_DIGITS COLOR_CYAN
+#define BG_DIGITS COLOR_WHITE
+
+// Colors for won game
+#define FG_WON COLOR_GREEN
+#define BG_WON COLOR_WHITE
+
+// Colors for initial numbers
+#define FG_INIT COLOR_YELLOW
+#define BG_INIT COLOR_WHITE
+
+// nicknames for pairs of colors
+enum { PAIR_BANNER = 1, PAIR_GRID, PAIR_BORDER, PAIR_LOGO, PAIR_DIGITS, PAIR_WON, PAIR_INIT };
